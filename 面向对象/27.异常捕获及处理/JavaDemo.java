615716e9bf1e1e4ef4536ee5cacd6ef61623194e
renjiandong <1398488165@qq.com>
2020-06-03 17:57:17 +0800

    java中的异常
    Exception

--- 面向对象/27.异常捕获及处理/JavaDemo.java
+++ 面向对象/27.异常捕获及处理/JavaDemo.java
@@ -1,19 +1,9 @@
 public class JavaDemo {
   public static void main(String args[]) {
-    System.out.println("【1】******程序执行开始");
     try{
-      int x = Integer.parseInt(args[0]);
-      int y = Integer.parseInt(args[1]);
-      System.out.println("【2】******数字计算" + ( x / y ));
-    }catch(ArithmeticException e) {
+      throw new Exception("自己抛着玩的异常对象");
+    }catch(Exception e) {
       e.printStackTrace();
-    }catch(NumberFormatException e) {
-      e.printStackTrace();
-    }catch(ArrayIndexOutOfBoundsException e) {
-      e.printStackTrace();
-    }finally {
-      System.out.println("【F】不管是否出现异常都执行");
     }
-    System.out.println("【3】******程序执行完毕");
   }
 }
